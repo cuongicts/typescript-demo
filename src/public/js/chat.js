fd5ac19e8e2cc7249b3841792c1ee6c8bf384c27
Cuong Nguyen <cuongnv@icts.vn>
2019-04-12 09:26:32 +0700

    update

--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -2,23 +2,45 @@ $(window).ready(function() {
   // create connection
   const url = 'wss://qizjzebghf.execute-api.us-east-2.amazonaws.com/FirstStage?name=Cuong';
   var ws = new WebSocket(url);
-  
-  var message = $('#message');
-  
   ws.onopen = (event) => console.log('connected');
   
-  ws.onmessage = data => {
-    console.log(`From server: ${data}`);
+  var message = $('#message'),
+      username = $('#username'),
+      btn = $('#send'),
+      output = $('#output'),
+      feedback = $('#feedback');
+
+  // emit events
+  btn.click(function() {
+    const body = {
+      action: 'sendmessage',
+      data: {
+        name: username.val(),
+        message: message.val()
+      },
+    };
+    ws.send(JSON.stringify(body));
+    message.val('');
+  });
+
+  // message.on('')
+  
+  ws.onmessage = response => {
+    let { data } = response;
+    data = JSON.parse(data);
+    feedback.text(data.message);
+    output.append(`<p><strong>${data.username}: </strong>${data.message}</p>`);
   };
+
   ws.onclose = () => {
     console.log('disconnected');
     process.exit();
   };
   
-  readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  }).on('line', data => {
-    ws.send(data);
-  });
+  // readline.createInterface({
+  //   input: process.stdin,
+  //   output: process.stdout,
+  // }).on('line', data => {
+  //   ws.send(data);
+  // });
 });
